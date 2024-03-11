beeba369d10e67ff4bfe2a06f79ad22d757186e8
TSSumit <tikonesum2001@gmail.com>
2024-03-12 00:09:54 +0530

    small updates in code of useContext and improvement in notes

--- src/utils/UserContext.js
+++ src/utils/UserContext.js
@@ -5,7 +5,7 @@ export const UserContext = createContext();
 
 export const UserProvider = ({ children }) => {
   const [user, setUser] = useState({
-    name: "name",
+    name: "",
     email: "email"
   });
 
